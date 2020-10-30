664b60a1380de02740cda8c53f52cdbe326a2817
Ulises López <ulises050794@gmail.com>
2020-10-30 17:55:56 -0600

    inital commit

---
+++ HolaSpring/src/main/java/ualb/com/HolaSpring/HolaSpringApplication.java
@@ -0,0 +1,14 @@
+package ualb.com.HolaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaSpringApplication.class, args);
+		System.out.println("hello word");
+	}
+
+}
