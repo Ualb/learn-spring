d95f1f08ca596ccc5ec420325bf8384f74340e4e
Ulises López <ulises050794@gmail.com>
2021-01-17 22:36:49 -0600

    se ha añadido agregar, editar y elimniar

--- SpringData/src/main/java/ualb/com/HolaSpring/InitialController.java
+++ SpringData/src/main/java/ualb/com/HolaSpring/InitialController.java
@@ -1,11 +1,16 @@
-package ualb.com.HolaSpring; 
+package ualb.com.HolaSpring;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
- 
+import org.springframework.web.bind.annotation.PostMapping;
+
 import ualb.com.HolaSpring.service.IPersonService; 
+import ualb.com.HolaSpring.domain.*;
 
 @Controller 
 public class InitialController {
@@ -20,5 +25,32 @@ public class InitialController {
         model.addAttribute("persons", persons);
 
         return "index";
+    }
+
+    @GetMapping("/agregar")
+    public String getAdd(Person person) {
+        return "modify";
+    }
+
+    @PostMapping("/save")
+    public String save(@Valid Person person, Errors errors) {
+        if (errors.hasErrors()) {
+            return "modify";
+        }
+        iPersonService.save(person);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{idPerson}")
+    public String edit(Person person, Model model) {
+        person = iPersonService.search(person);
+        model.addAttribute("person", person);
+        return "modify";
+    }
+
+    @GetMapping("/del/")
+    public String del(Person person) { 
+        iPersonService.delete(person);
+        return "redirect:/";
     }
 }
