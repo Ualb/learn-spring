1c9755373a4502152e153862d576dfe8e31348dd
Ulises López <ulises050794@gmail.com>
2021-01-20 19:20:01 -0600

    interceptor del tipo de lenguage

---
+++ SpringData/src/main/java/ualb/com/HolaSpring/WebConfig.java
@@ -0,0 +1,34 @@
+package ualb.com.HolaSpring;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        var sessionLocalResolver = new SessionLocaleResolver();
+        sessionLocalResolver.setDefaultLocale(new Locale("es"));
+        return sessionLocalResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
