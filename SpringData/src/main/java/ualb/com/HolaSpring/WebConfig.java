1b35656e72d9f31fb6cd4774f0404ff0c61b2b0c
Ulises López <ulises050794@gmail.com>
2021-01-21 19:54:47 -0600

    se ha agregado un controlador para el root, login y errores

--- SpringData/src/main/java/ualb/com/HolaSpring/WebConfig.java
+++ SpringData/src/main/java/ualb/com/HolaSpring/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -30,5 +31,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login");
+        registry.addViewController("/errors/403");
     }
 }
