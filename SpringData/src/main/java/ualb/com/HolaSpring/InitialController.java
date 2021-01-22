6ff4bfcea00f909845cce6211c3093a87c349d82
Ulises López <ulises050794@gmail.com>
2021-01-21 19:57:05 -0600

    import de user agregada

--- SpringData/src/main/java/ualb/com/HolaSpring/InitialController.java
+++ SpringData/src/main/java/ualb/com/HolaSpring/InitialController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
